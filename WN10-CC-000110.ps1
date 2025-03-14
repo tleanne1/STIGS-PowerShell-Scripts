d94c3c063baf27921d0496f2e571902b1c4100cc
Tracey B <114450881+tleanne1@users.noreply.github.com>
2025-03-14 12:52:48 -0500

    Create WN10-CC-000110.ps1

---
+++ WN10-CC-000110.ps1
@@ -0,0 +1,42 @@
+ <#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Tracey B
+    LinkedIn        : linkedin.com/in/tleanne/
+    GitHub          : github.com/tleanne1
+    Date Created    : 2025-03-14
+    Last Modified   : 2025-03-14
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000110
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(WN10-CC-000110).ps1 
+#>
+
+# Define the registry path and value
+$RegistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers'
+$ValueName = 'DisableHTTPPrinting'
+$ValueData = 1  # Disable HTTP Printing
+
+# Check if the registry path exists; if not, create it
+if (-not (Test-Path $RegistryPath)) {
+    New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT' -Name 'Printers' -Force
+}
+
+# Set the registry value
+New-ItemProperty -Path $RegistryPath -Name $ValueName -Value $ValueData -PropertyType DWord -Force
+
+Write-Host "HTTP printing has been disabled."
+ 
